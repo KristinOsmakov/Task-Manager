3d1a27989935f676e50ebfe8edc435800cb184ed
KristinOsmakov <oskristinov@gmail.com>
2024-08-24 15:55:31 +0300

    Todolist, friday 2, with error in tasks.reducer.test.ts and not api.

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
   headers: {
-    "API-KEY": "32511c99-28de-44d1-9df6-b2ae2555b019",
+    "API-KEY": process.env.REACT_APP_API_KEY,
   },
 });
